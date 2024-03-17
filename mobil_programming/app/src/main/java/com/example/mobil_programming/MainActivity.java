36e15027009b86332b852dfd3fd15318ec71818f
Muharrem Aslan <muharrem5352@gmail.com>
2024-03-18 02:14:52 +0300

    added work3

--- mobil_programming/app/src/main/java/com/example/mobil_programming/MainActivity.java
+++ mobil_programming/app/src/main/java/com/example/mobil_programming/MainActivity.java
@@ -16,12 +16,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void goWork(View view) {
+
         int objId = view.getId();
 
         if (R.id.btn_work1 == objId) {
             Intent work1Intent = new Intent(MainActivity.this, work1.class);
              startActivity(work1Intent);
-        } else {
+        }
+        else if (R.id.btn_work2 == objId) {
+
+            Intent work2Intent = new Intent(MainActivity.this, work1.class);
+            startActivity(work2Intent);
+        }
+        else if (R.id.btn_work3 == objId) {
+
+            Intent work2Intent = new Intent(MainActivity.this, work2.class);
+            startActivity(work2Intent);
+        }
+        else {
             System.out.println("Warning");
         }
     }
